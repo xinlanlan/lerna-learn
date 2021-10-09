62e033d11c624fde205bba7afd0921aab1c7736d
史少飞 <shishaofei@shishaofeideMacBook-Pro.local>
2021-10-09 11:53:28 +0800

    fix: 212

--- packages/ssf-lerna-1/index.js
+++ packages/ssf-lerna-1/index.js
@@ -1,3 +1,5 @@
 #!/usr/bin/env node
 console.log(111)
 console.log(333)
+const add = require('ssf-lerna-2')
+console.log(add(100,222))
--- packages/ssf-lerna-2/index.js
+++ packages/ssf-lerna-2/index.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
 console.log(334)
+
+function add(a, b) {
+    return a + b
+}
+
+module.exports = add;
