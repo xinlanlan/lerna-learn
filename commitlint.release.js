9a28a3221f88e61fc5968cb07d2a72d065436e49
史少飞 <shishaofei@shishaofeideMacBook-Pro.local>
2021-12-08 02:47:02 +0800

    build: 修改配置

--- commitlint.release.js
+++ commitlint.release.js
@@ -2,10 +2,15 @@ const shell = require('shelljs');
 const version = require('./lerna').version;
 console.log(version)
 
-// const main = async () => {
-//     await shell.exec('git add -A');
-//     await shell.exec(`git commit -m "chore: publish ${version}"`);
-//     await shell.exec('git push');
-// };
+const main = async () => {
 
-// main();
+    const isAlpha = !!(version.indexOf('alpha') > -1);
+
+    if (isAlpha) {
+        await shell.exec('git add -A');
+        await shell.exec(`git commit -m "chore: prerelease ${version}"`);
+        await shell.exec('git push');
+    }
+};
+
+main();
